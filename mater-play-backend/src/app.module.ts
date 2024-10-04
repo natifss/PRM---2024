05c39e44dc4ff7f6bfc3578f1c3c76dde7fbc5e2
Nati <natali-.santos@hotmail.com>
2024-10-03 22:09:28 -0300

    Aula 03-10-2024

---
+++ mater-play-backend/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      entities: [],
+      synchronize: true,
+    })
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
